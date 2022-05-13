6e01aaaaee24eeeef3ca4622a2f65bf626ea9aac
Johnathan <jsdunnavant@gmail.com>
2022-05-12 21:54:27 -0400

    updated cards and working on second array

--- repos.js
+++ repos.js
@@ -22,23 +22,25 @@ const renderToDom = (divId, textToRender) => {
   selectedElement.innerHTML = textToRender
 };
 
-const myRepoCards = () => {
-  let domString = "";
-  domString = `
-  <div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-  </div>`;
-  renderToDom("#repoContainer", domString)
-}
+// const myRepoCards = () => {
+//   let domString = "";
+//   for (const card of repos) {
+//   domString = `
+//   <div class="card" style="width: 18rem;">
+//   <div class="card-body">
+//     <h5 class="card-title">Card title</h5>
+//     <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+//     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//     <a href="#" class="card-link">Card link</a>
+//     <a href="#" class="card-link">Another link</a>
+//   </div>
+//   </div>`;
+// }
+//   renderToDom("#repoContainer", domString)
+// }
 
 
-const startApp = () => {
-  myRepoCards();
-}
-startApp();
+// const startApp = () => {
+//   myRepoCards(repos);
+// }
+// startApp();
