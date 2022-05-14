591d79301b2f6b9470914f6a647d93e39fe55a71
Ana Luna <102691203+alunag963@users.noreply.github.com>
2022-05-14 13:10:24 -0500

    add rectangle 3 and html

--- project.js
+++ project.js
@@ -1,10 +1,55 @@
+//5 projects loop//
+
+const rectangle = [
+    {
+        name: "Project 1",
+        description: "no description",
+        type: "Private",
+        time: "Update 4 mins ago",
+    },
+
+
+    {
+        name: "Project 2",
+        description: "Goals for NSS Bootcamp",
+        type: "Public",
+        time: "Updated 17 days ago",
+    },
+
+
+    {
+        name: "Project 3",
+        description: "no description",
+        type: "Public",
+        time: "Updated 37 mins ago",
+    },
+
+    {
+        name: "Project 4",
+        description: "Practice for inclass",
+        type: "Private",
+        time: "Updated 16 hours ago",
+    },
+
+    {
+        name: "Project 5",
+        description: "Project Practice",
+        type: "Public",
+        time: "Updated 27 hours ago",
+    },
+
+];
+
+
+
+
+
 
 //Keep shit clean!//
 const renderToDom = (divId, textToRender) => {
     const selectedElement = document.querySelector(divId);
     selectedElement.innerHTML = textToRender
 };
-
 
 
 
@@ -16,32 +61,37 @@ const rectangle1 = (yomama) => {
     for (const item of yomama) {
         domString += `
         <div class="card" style="width: 18rem;">
-  <div class="card-header">
-    Open    Closed
-    <div class="btn-group" role="group">
-    <button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-      Sort
-    </button>
-    <ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-      <li><a class="dropdown-item" href="#">Newest</a></li>
-      <li><a class="dropdown-item" href="#">Oldest</a></li>
-      <li><a class="dropdown-item" href="#">Recently Updatedk</a></li>
-      <li><a class="dropdown-item" href="#">Least recently updated</a></li>
-      <li><a class="dropdown-item" href="#">Name</a></li>
-    </ul>
-  </div>
-</div>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">Project1</li>
-    <li class="list-group-item">Project2</li>
-    <li class="list-group-item">Project3</li>
-    <li class="list-group-item">Project4</li>
-    <li class="list-group-item">Project5</li>
+        <div class="card-header">
+        Open  
+         <div class="btn-group" role="group">
+        <button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+        Sort
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+        <li><a class="dropdown-item" href="#">Newest</a></li>
+        <li><a class="dropdown-item" href="#">Oldest</a></li>
+        <li><a class="dropdown-item" href="#">Recently Updated</a></li>
+        <li><a class="dropdown-item" href="#">Least recently updated</a></li>
+        <li><a class="dropdown-item" href="#">Name</a></li>
+        </ul>
+        </div>
+        </div>
+        </div>
+        <ul class="list-group list-group-flush">
+        <li class="list-group-item">${item.name}</li>
+        <p class="card-text">${item.description}</p>
+        <p class="card-text">${item.type}</p>
+        <p class="card-text">${item.time}</p>
 
-  </ul>
-</div>`;
-    }
+
+      
+
+             </ul>
+                </div>`;
+ 
+            }
 }
-renderToDom(`#rectangle1`, domString);
+renderToDom("#rectangle1", domString);
 
+
+rectangle1()
