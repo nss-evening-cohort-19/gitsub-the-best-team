fe08f03bdc56f97f6f5f44d8cf970a10036d67c9
Johnathan <jsdunnavant@gmail.com>
2022-05-15 21:48:56 -0400

    changes

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 import { packagesList, packagesCardsOnDom, newPackageForm, packagesEventListeners } from "./packagesPage.js";
-import { repos, myRepoCards, myRepoForm, repoEventListeners, startApp} from "./repos.js"
+import { repos, myRepoCards, myRepoForm, repoEventListeners, startApp } from "./repos.js";
 
 export const renderToDom = (divId, textToRender) => {
   const selectedElement = document.querySelector(divId);
   selectedElement.innerHTML = textToRender;
 };
+const startApp = () => {
+  myRepoCards(repos);
+  myRepoForm();
+  repoEventListeners();
+};
+startApp();
